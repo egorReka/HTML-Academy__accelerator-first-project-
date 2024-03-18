64fe52fc010973c60a45d92f07179121c8f071d7
egorReka <e.reka55@yandex.ru>
2024-03-18 20:29:20 +0300

    feat: add about

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,7 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+import { initVideo } from './video';
+
+initVideo();
---
+++ source/js/video.js
@@ -0,0 +1,35 @@
+const LINK_VIDEO = 'https://www.youtube.com/embed/';
+const ID_VIDEO = '9TZXsZItgdw';
+const QWERY_PARAMS = '?rel=0&showinfo=0&autoplay=1';
+
+const videoContainer = document.querySelector('.video');
+const videoLink = videoContainer.querySelector('.video__link');
+const videoButton = videoContainer.querySelector('.video__button');
+
+const createIframe = () => {
+  const fragment = document.createDocumentFragment();
+  const iframe = document.createElement('iframe');
+
+  iframe.setAttribute('allow', 'autoplay; fullscreen');
+  iframe.setAttribute('anonymous', '');
+  iframe.setAttribute('src', LINK_VIDEO + ID_VIDEO + QWERY_PARAMS);
+  iframe.classList.add('video__media');
+  fragment.append(iframe);
+
+  return fragment;
+};
+
+const setupVideo = () => {
+  videoLink.remove();
+  videoButton.remove();
+  videoContainer.append(createIframe());
+};
+
+const initVideo = () => {
+  videoLink.removeAttribute('href');
+  videoContainer.classList.add('video--enabled');
+
+  videoContainer.addEventListener('click', setupVideo, { once: true });
+};
+
+export { initVideo };
