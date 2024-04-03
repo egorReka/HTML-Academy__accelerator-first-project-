3ac137c801d25d89aa319295d65e97c381f3eea0
egorReka <e.reka55@yandex.ru>
2024-04-03 16:02:26 +0300

    feat: add in sessionStorage status

---
+++ source/js/faq.js
@@ -0,0 +1,65 @@
+import { ASKED_QUESTIONS } from './data.js';
+
+const categoriesContainer = document.querySelector('.faq-categories');
+const faqList = document.querySelector('.faq-list');
+const checkboxes = faqList.querySelectorAll('.faq-list__checkbox');
+const questions = faqList.querySelectorAll('.faq-list__checkbox ~ label');
+const answers = faqList.querySelectorAll('.faq-list__content-wrapper p');
+
+const updateStatusCheckboxes = () => {
+  const сategories = categoriesContainer.querySelectorAll('.faq-categories__radio');
+  const storageStatus = [];
+
+  сategories.forEach((сategory) => {
+    const isCurrentCategory = сategory.checked === true;
+
+    if (isCurrentCategory) {
+      checkboxes.forEach((checkbox) => {
+        storageStatus.push(checkbox.checked);
+      });
+
+      sessionStorage.setItem(сategory.value, storageStatus);
+    }
+  });
+};
+
+const closeСheckboxes = () => {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+};
+
+const openSavedСheckboxes = (targetCategories) => {
+  const isCategorySaved = sessionStorage.getItem(targetCategories);
+
+  if (isCategorySaved) {
+    checkboxes.forEach((checkbox, i) => {
+      const savedStatus = JSON.parse(sessionStorage.getItem(targetCategories).split(',')[i]);
+
+      checkbox.checked = savedStatus;
+    });
+  }
+};
+
+const changCategory = (evt) => {
+  const targetCategories = evt.target.value;
+
+  closeСheckboxes();
+  openSavedСheckboxes(targetCategories);
+
+  for (let i = 0; i < ASKED_QUESTIONS[targetCategories].length; i++) {
+    const { question, answer } = ASKED_QUESTIONS[targetCategories][i];
+
+    questions[i].childNodes[0].textContent = question;
+    answers[i].textContent = answer;
+  }
+};
+
+const initFaq = () => {
+  window.addEventListener('scroll', updateStatusCheckboxes, { once: true });
+
+  categoriesContainer.addEventListener('change', changCategory);
+  faqList.addEventListener('change', updateStatusCheckboxes);
+};
+
+export { initFaq };
--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,11 @@
 import { initVideo } from './video.js';
 import { initSelectPrice } from './select-price.js';
 import { initJuriSwiper } from './juri-swiper.js';
-import { initSelectFaq } from './select-faq.js';
+import { initFaq } from './faq.js';
 import { initReviewsSwiper } from './reviews-swiper.js';
 
 initVideo();
 initSelectPrice();
 initJuriSwiper();
-initSelectFaq();
+initFaq();
 initReviewsSwiper();
